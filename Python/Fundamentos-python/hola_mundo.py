64f1d2f30ea97dc47d456b168271e7e69bbb7538
Albert Contreras <albertmendoza019@gmail.com>
2024-10-13 19:48:32 -0600

    Se añadieron comentarios

--- Python/Fundamentos-python/hola_mundo.py
+++ Python/Fundamentos-python/hola_mundo.py
@@ -1,2 +1,11 @@
+# Albert Alexis Contreras Mendoza
+# 9 de Octubre de 2024
+# En este programa, se imprime el mensaje <Hola mundo>
+
+"""
+La función print() se utiliza para mostrar información en la consola o terminal.
+El mensaje puede ser una cadena o cualquier otro objeto; el objeto se convertirá en una cadena antes de escribirse en la pantalla.
+El mensaje se puede colocar dentro de comillas dobles o simples, no hay diferencia.
+"""
 print("Hola mundo! :D")
 print('Hola mundo! :D')
--- Python/Fundamentos-python/uso_de_variables.py
+++ Python/Fundamentos-python/uso_de_variables.py
@@ -1,21 +1,21 @@
 # Albert Alexis Contreras Mendoza
-# 7 de septiembre de 2024
+# 9 de Octubre de 2024
 # En este archivo se ejemplifica el uso de variables en Python.
 
 # Notas:
-# En Python to.do es un objeto.
+# En Python toodo es un objeto.
 # Variable - una variable es un nombre que almacena un valor guardado en la memoria temporal.
 
 # Toda variable requiere un valor inicial
 semestre = 3        # es una variable que apunta a un objeto de tipo int con valor de 3
 print(semestre)     # imprime el valor de la variable
-semestre = 4        # la variable ya no apunta al objeto anterior, sino a uno nuevo, por lo que se pierde la referencia
+semestre = 5        # la variable ya no apunta al objeto anterior, sino a uno nuevo, por lo que se pierde la referencia
 print(semestre)
 
 # Se crean varias variables para ejemplificar su uso
-nombre = "Alberto"  # variable de tipo String
-altura = 1.65       # variable de tipo Float
-edad = 31           # variable de tipo Int
+nombre = "Albert"  # variable de tipo String
+altura = 1.62       # variable de tipo Float
+edad = 20           # variable de tipo Int
 
 # Se imprimen las variables, añadiendo información adicional para comprender lo que se imprime
 print("Nombre:", nombre)
@@ -24,8 +24,8 @@ print("Altura: ", altura, "m.")
 print("Edad: ", edad, "años.")
 
 # Se modifican los valores de las variables y se mandan a imprimir
-altura = 1.66
-edad = 30
+altura = 1.63
+edad = 21
 print()
 print("Valores modificados:")
 print("Nombre:", nombre)
@@ -34,7 +34,7 @@ print("Altura: ", altura, "m.")
 print("Edad: ", edad, "años.")
 
 # En Python, las variables son dinámicas, por lo que pueden almacenar otro tipo de dato en cualquier momento
-edad = "treinta y uno"      # edad antes tenía el valor de 31 (Int)
+edad = "veintiuno"      # edad antes tenía el valor de 21 (Int)
 print()
 print("Edad (con otro tipo de dato):", edad)
 
@@ -50,18 +50,18 @@ print("Edad (con otro tipo de dato):", edad)
 # - Utilizar nombres descriptivos de acuerdo a su uso. Por ejemplo: edad, en lugar de e.
 
 # Ejemplos correctos y con buenas prácticas
-fecha_nacimiento = "1 de enero dl 2000"
+fecha_nacimiento = "19 de abril del 2004"
 clase = "Paradigmas de programación II"
 horas_estudio = 8
-nombre = "Alberto"
+nombre = "Albert"
 es_estudiante = True
 
 # Ejemplos incorrectos (líneas comentadas porque marcan error) o de malas prácticas
-f = "1 de enero del 2000"
-fechanacimiento = "1 de enero del 2000"
+f = "19 de abril del 2004"
+fechanacimiento = "19 de abril del 2004"
 # class = "Paradigmas de programación II"
 # 8horas_estudio = 8
-Nombre = "A l b e r t o"
+Nombre = "A l b e r t"
 NOMBRE = "ALBERTO"
 
 # Notar que las variables 'nombre', 'Nombre' y 'NOMBRE', son distintas
