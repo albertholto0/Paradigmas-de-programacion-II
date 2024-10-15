280f7b71d898d926a5cc51899a6f0ae45a9b617c
Albert Contreras <albertmendoza019@gmail.com>
2024-10-14 20:17:07 -0600

    Se añadieron comentarios

---
+++ Python/Fundamentos-python/tipos_de_datos_ejercicio.py
@@ -0,0 +1,30 @@
+"""
+Albert Alexis Contreras Mendoza
+12 de Octubre del 2024
+Ejercicio aplicando lo visto en la lección << Tipo de datos >>
+"""
+
+dia = "Lunes"                   #Cadena de texto
+pasaje = 20                     #Entero
+comida = 135.5                  #Flotante
+otros_gastos = 34.5             #Flotante
+presupuesto_booleano = True     #Booleano
+
+print("*** Gastos diarios ***")
+print("Dia: ", dia)
+print("Pasaje: ", pasaje)
+print("Comida: ", comida)
+print("Otros gastos: ", otros_gastos)
+print("¿Fue mayor a mi presupuesto: ", presupuesto_booleano)
+
+dia = "Martes"                  #Cadena de texto
+pasaje = 12                     #Entero
+comida = 45.5                   #Flotante
+otros_gastos = 4.5              #Flotante
+presupuesto_booleano = False    #Booleano
+
+print("*** Gastos diarios ***")
+print("Dia: ", dia)
+print("Pasaje: ", pasaje)
+print("Comida: ", comida)
+print("Otros gastos: ", otros_gastos)
