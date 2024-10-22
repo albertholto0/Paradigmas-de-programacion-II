31c9f4900f7068513789889c6141b2cdc1f0e7ee
Albert Contreras <albertmendoza019@gmail.com>
2024-10-21 22:07:09 -0600

    Se actualizaron comentarios

--- Python/Fundamentos-python/entrada_consola.py
+++ Python/Fundamentos-python/entrada_consola.py
@@ -1,21 +1,25 @@
 '''
 Nombre: Albert Alexis Contreras Mendoza
 Fecha: 15 de octubre del 2024
-Descripción: Entrada de datos por consola para interacturar con el usuario con valores dinámicos.
+Descripción: Entrada de datos por consola para interactuar con el usuario con valores dinámicos.
 '''
 
-# Comentar sobre la función input.
+#La función input() obtiene una entrada de texto del usuario.
 numero1_cadena = input("Introduce un número decimal: ")
 numero2_cadena = input("Introduce otro número decimal: ")
-resultado_cadena = numero1_cadena + numero2_cadena # Verificar qué es lo que realiza esta instrucción (ver el print).
+
+#Al sumar cadenas, los valores se concatenan (se unen como texto).
+resultado_cadena = numero1_cadena + numero2_cadena
 print()
-print(" ****  Recibir número sin un casting de varibles  ****")
+print(" ****  Recibir número sin un casting de variables  ****")
 print(f"El resultado de {numero1_cadena} y {numero2_cadena} es: {resultado_cadena}")
 
-# Comentar por qué se realiza el casting de variables.
+#El casting convierte las cadenas a números decimales para realizar operaciones matemáticas.
 numero1_float = float(numero1_cadena)
 numero2_float = float(numero2_cadena)
-resultado_float = numero1_float + numero2_float # Verificar qué es lo que realiza de esta manera y compáralo.
+
+#Aquí, la suma es matemática porque las variables son de tipo float.
+resultado_float = numero1_float + numero2_float
 print()
-print(" ****  Casting de varibles  ****")
+print(" ****  Casting de variables  ****")
 print(f"El resultado de {numero1_float} y {numero2_float} es: {resultado_float}")
