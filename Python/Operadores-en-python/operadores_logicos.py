c22d3d07128c397999f0cb10b2e97973f0575fb3
Albert Contreras <albertmendoza019@gmail.com>
2024-10-28 10:46:48 -0600

    Se añadieron comentarios

--- Python/Operadores-en-python/operadores_logicos.py
+++ Python/Operadores-en-python/operadores_logicos.py
@@ -1,9 +1,24 @@
+"""
+Nombre: Albert Alexis Contreras Mendoza
+Fecha: 18 de Octubre del 2024
+Descripción: Ejemplos de uso de los operadores relacionales.
+"""
+
+"""
+Los operadores lógicos permiten combinar expresiones booleanas (verdadero o falso) para crear condiciones más complejas.
+Estos operadores nos permiten realizar operaciones lógicas como "y", "o" y "no", lo que nos da la capacidad de tomar 
+decisiones más sofisticadas dentro de nuestros programas.
+"""
+
+# Se solicita por consola que se ingresen dos valores (Si/No) para covnertirlas a expresiones booleanas.
 p = input("p = ingresa si/no: ")
 p = p.lower() == "si"
 
+# Las cadenas se convierten a expresiones booleanas (ver Fundamentos-Python -> Entrada_conversiones.py).
 q = input("q = ingresa si/no: ")
 q = q.lower() == "si"
 
+# Se imprimen mensajes sobre operaciones lógicas.
 print(f"p and q: {p and q}")
 print(f"p or q: {p or q}")
 print(f"not p: {not p}")
--- Python/Operadores-en-python/operadores_relacionales.py
+++ Python/Operadores-en-python/operadores_relacionales.py
@@ -1,3 +1,26 @@
+"""
+Nombre: Albert Alexis Contreras Mendoza
+Fecha: 18 de Octubre del 2024
+Descripción: Ejemplos de uso de los operadores relacionales.
+"""
+
+"""
+Los operadores relacionales son símbolos que se utilizan en programación para comparar dos valores. 
+Estos operadores devuelven un valor booleano (verdadero o falso) dependiendo del resultado de la comparación. 
+Son fundamentales para tomar decisiones dentro de un programa, ya que permiten construir expresiones condicionales 
+que determinan el flujo de ejecución.
+
+Operadores Relacionales Comunes:
+Operador	Significado
+==	Igual a
+!=	Diferente de
+>	Mayor que
+<	Menor que
+>=	Mayor o igual que
+<=	Menor o igual que
+"""
+
+# Se solicitan dos números para realizar distintas operaciones relacionales.
 num_a = float(input("Ingresa un numero: "))
 num_b = float(input("Ingresa otro: "))
 
