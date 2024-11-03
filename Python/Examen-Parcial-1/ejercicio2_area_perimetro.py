c02cb74ed8dbd1ba21c62692d0dc740c9bdfde6d
Albert Contreras <albertmendoza019@gmail.com>
2024-11-03 00:59:17 -0600

    Se modificaron los leteros para que solamente imprimieran números con 3 decimas

--- Python/Examen-Parcial-1/ejercicio2_area_perimetro.py
+++ Python/Examen-Parcial-1/ejercicio2_area_perimetro.py
@@ -15,20 +15,20 @@ while flag != 0:
         print("+ - - CALCULAR ÁREA DE UN RECTÁNGULO - - +")
         base = float(input("Ingrese [base]: "))
         altura = float(input("Ingrese [altura]: "))
-        print(f"El area resultante es: {base * altura}")
+        print(f"El area resultante es: {base * altura:.3f}")
     elif op == 2:
         print("+ - - CALCULAR PERÍMETRO DE UN RECTÁNGULO - - +")
         base = float(input("Ingrese [base]: "))
         altura = float(input("Ingrese [altura]: "))
-        print(f"El perimetro resultante es: {base + altura + base + altura}")
+        print(f"El perimetro resultante es: {(2 * base + 2 * altura):.3f}")
     elif op == 3:
         print("+ - - CALCULAR ÁREA DE UN CÍRCULO - - +")
         radio = float(input("Ingrese [radio]: "))
-        print(f"El area resultante es: {(radio ** 2) * 3.1416}")
+        print(f"El area resultante es: {(radio ** 2) * 3.1416:.3f}")
     elif op == 4:
         print("+ - - CALCULAR PERÍMETRO DE UN CÍRCULO - - +")
         radio = float(input("Ingrese [radio]: "))
-        print(f"El perimetro resultante es: {radio * 2 * 3.1416}")
+        print(f"El perimetro resultante es: {radio * 2 * 3.1416:.3f}")
     elif op == 0:
         # Si el usuario ingresa '0' (donde el menú dice que 0 es para salir),
         # la bandera toma el valor de '0', por lo tanto el programa no puede seguir funcionando
