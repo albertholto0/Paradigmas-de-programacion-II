bc844c85af91084ba702b2af21606bc855ca10ed
Albert Contreras <albertmendoza019@gmail.com>
2025-01-09 12:43:43 -0600

    Ahora hay 4 patrones diferentes de mapas. Se selecciona uno de manera aleatoria

--- Python/videojuego_tanque/tank-battle-city_6/config.py
+++ Python/videojuego_tanque/tank-battle-city_6/config.py
@@ -1,4 +1,4 @@
-#import random
+import random
 
 class Config:
     """Constructor"""
@@ -8,56 +8,55 @@ class Config:
         self.screen_height = 720
         self.background_image_path = "media/fondo.jpg"
         self.fps = 30
-
         self.tank_speed = 7
-
         self.bala_speed = 17
-        #self.paredes = self.generar_paredes_aleatorias()
+        self.paredes = self.seleccionar_paredes_aleatorias()
 
-        """
-        self.paredes = [
-            [300, 100, 50, 200],  # Pared vertical izquierda
-            [700, 100, 50, 200],  # Pared vertical derecha
-            [300, 500, 50, 200],  # Pared vertical inferior izquierda
-            [700, 500, 50, 200],  # Pared vertical inferior derecha
-            [425, 300, 200, 50],  # Pared horizontal central
+    def seleccionar_paredes_aleatorias(self):
+        patrones = [
+            [
+                [300, 100, 50, 200],  # Pared vertical izquierda superior
+                [700, 100, 50, 200],  # Pared vertical derecha superior
+                [300, 500, 50, 200],  # Pared vertical inferior izquierda
+                [700, 500, 50, 200],  # Pared vertical inferior derecha
+                [425, 300, 200, 50]  # Pared horizontal central
+            ],
+            [
+                [280, 100, 50, 200],  # Pared vertical izquierda superior
+                [700, 100, 50, 200],  # Pared vertical derecha superior
+                [300, 500, 50, 200],  # Pared vertical inferior izquierda
+                [700, 500, 50, 200],  # Pared vertical inferior derecha
+                [400, 300, 200, 50],  # Pared horizontal central
+                [170, 250, 50, 150],  # Pared vertical izquierda pequeña
+                [810, 250, 50, 150],  # Pared vertical derecha pequeña
+                [500, 150, 50, 150],  # Pared vertical central superior
+                [550, 450, 50, 150],  # Pared vertical central inferior
+                [500, 100, 200, 50]  # Pared horizontal superior
+            ],
+            [
+                [150, 100, 50, 200],  # Pared vertical izquierda superior
+                [900, 100, 50, 200],  # Pared vertical derecha superior
+                [150, 460, 50, 200],  # Pared vertical izquierda inferior
+                [900, 460, 50, 200],  # Pared vertical derecha inferior
+                [470, 350, 200, 50],  # Pared horizontal central
+                [360, 200, 50, 150],  # Pared vertical izquierda central
+                [730, 200, 50, 150],  # Pared vertical derecha central
+                [470, 550, 200, 50],  # Pared horizontal inferior
+            ],
+            [
+                [330, 150, 50, 150],  # Pared vertical izquierda
+                [700, 150, 50, 150],  # Pared vertical derecha
+                [330, 450, 50, 150],  # Pared vertical inferior izquierda
+                [700, 450, 50, 150],  # Pared vertical inferior derecha
+                [440, 320, 200, 50],  # Pared horizontal central
+                [440, 100, 200, 50],  # Pared horizontal superior
+                [440, 600, 200, 50],  # Pared horizontal inferior
+
+            ],
         ]
-        """
 
-        self.paredes = [
-            [300, 100, 50, 200],  # Pared vertical izquierda
-            [700, 100, 50, 200],  # Pared vertical derecha
-            [300, 500, 50, 200],  # Pared vertical inferior izquierda
-            [700, 500, 50, 200],  # Pared vertical inferior derecha
-            [400, 300, 200, 50],  # Pared horizontal central
-            [200, 250, 50, 150],  # Pared vertical izquierda pequeña
-            [800, 250, 50, 150],  # Pared vertical derecha pequeña
-            [500, 150, 50, 150],  # Pared vertical central
-            [550, 450, 50, 150],  # Pared vertical inferior
-            [500, 100, 200, 50],  # Pared horizontal superior
-        ]
+        indice_patron = random.randint(0, len(patrones) - 1)
+        patron_seleccionado = patrones[indice_patron]
+        print(f"Patrón seleccionado: {indice_patron + 1}")
 
-    """
-    def generar_paredes_aleatorias(self):
-        paredes = []
-        num_paredes = random.randint(5, 10)  # Generamos entre 5 y 10 paredes
-
-        for _ in range(num_paredes):
-            # Determinamos si la pared será vertical u horizontal
-            orientacion = random.choice(['vertical', 'horizontal'])
-
-            if orientacion == 'vertical':
-                x = random.randint(0, self.screen_width - 50)
-                y = random.randint(0, self.screen_height - 200)
-                width = 50
-                height = random.randint(100, 200)
-            else:
-                x = random.randint(0, self.screen_width - 200)
-                y = random.randint(0, self.screen_height - 50)
-                width = random.randint(100, 200)
-                height = 50
-
-            paredes.append((x, y, width, height))
-
-        return paredes
-    """
+        return patron_seleccionado
